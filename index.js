a762ef319f85f9e1717982d11b2d6af3cf492681
Abdelrahman Ibrahim <abdelrahman.elbatal19@gmail.com>
2024-05-18 23:55:28 +0300

    change content for the gazillionth time

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>pika pika</h1>')
+  res.send('<h1>pika pika one last dance</h1>')
 })
 
 const PORT = 8080
